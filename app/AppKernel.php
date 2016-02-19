a154e4ffe7dc4bfbc41550d68bd22444ea5e48f2
BaptisteLeBescond <baptiste.lb@gmail.com>
2016-02-19 10:18:01 +0100

    Add BibliBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new PlatformBundle\PlatformBundle(),
+            new BibliBundle\BibliBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
