280b7aae0183d7e0a3bbca99d1699543a65ad212
jjny <jminfir@gmail.com>
2016-02-19 10:27:39 +0100

    add notificationBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new PlatformBundle\PlatformBundle(),
             new BibliBundle\BibliBundle(),
+            new NotificationBundle\NotificationBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
